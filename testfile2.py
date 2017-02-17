df73304ea9005c370ab402346f771b72bb467a08
dung <cucarrot97@gmail.com>
2017-02-17 17:34:42 +0700

    testfile2

---
+++ testfile2.py
@@ -0,0 +1,10 @@
+def testfunction2():
+	return 'this is test function 2'
+
+class a:
+	def p(self):
+		print 'this is class a, testfile2'
+
+class b:
+	def p(self):
+		print 'this is class b, testfile2'
